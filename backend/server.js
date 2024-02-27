03b528e42478024c16fffaecac7b848852897fb2
inas <inaswendem.com>
2024-02-27 03:40:32 -0800

    folder added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const dotenv= require('dotenv').config(); //process.env
 const port= process.env.PORT;
 
+connectDB();
+
 const app = express();
+
+// middleware
+app.use(express.json()); 
+app.use(express.urlencoded({extended:false}));
+
+// routes
+
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
